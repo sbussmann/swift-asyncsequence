2ebd9b50774aded73c1435429d7180954ab82cdd
Shane Bussmann <sbussmann@gmail.com>
2024-11-15 20:58:00 -0700

    Initial commit of app.

---
+++ SwiftAsyncSequence/SwiftAsyncSequence/SwiftAsyncSequenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAsyncSequenceApp.swift
+//  SwiftAsyncSequence
+//
+//  Created by Shane Bussmann on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAsyncSequenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
